2794de40facff2237151cef0339419a9a5f4d561
Andrew Carpenter <andrewcarpenter87@gmail.com>
2013-05-30 14:51:38 +0100

    Grouped reactive subs

--- client/main.js
+++ client/main.js
@@ -3,11 +3,17 @@ myFollowsHandle = Meteor.subscribe('follows', Meteor.userId());
 timelineHandle = Meteor.subscribeWithPagination('timeline',10);
 
 Meteor.autorun(function(){
-	postRepliesHandle = Meteor.subscribe('replies', Session.get('currentPostId'));
-	currentPostHandle = Meteor.subscribe('currentPost', Session.get('currentPostId'));
+	var currentPostId = Session.get('currentPostId');
+	postRepliesHandle = Meteor.subscribe('replies', currentPostId);
+	currentPostHandle = Meteor.subscribe('currentPost', currentPostId);
+});
 
-	userFollowsHandle = Meteor.subscribe('follows', Session.get('currentUserId'));
-	userPostsHandle = Meteor.subscribe('userPosts', Session.get('currentUserId'));
+Meteor.autorun(function(){
+	var currentUserId = Session.get('currentUserId');
+	userFollowsHandle = Meteor.subscribe('follows', currentUserId);
+	userPostsHandle = Meteor.subscribe('userPosts', currentUserId);
+});
 
+Meteor.autorun(function(){
 	userSearchHandle = Meteor.subscribe('userSearch', Session.get('searchQuery'));
 });
